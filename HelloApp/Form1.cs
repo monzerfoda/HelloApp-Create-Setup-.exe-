454c54270629dd199a358010798a873711857436
monzerfoda <monzerfoda@gmail.com>
2022-06-30 01:33:45 +0200

    Add project files.

---
+++ HelloApp/Form1.cs
@@ -0,0 +1,17 @@
+namespace HelloApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("MFodaTech");
+        }
+    }
+}
